3c152b80ac281b8a3c8cd60d34ff1a75e314e4cb
Jake Linden <lindenja95@gmail.com>
2015-02-25 17:48:06 -0500

    Updated the formatting of the return for the getRank method and additionally fixed an indexing error in the getRank method.

--- textedu/src/textedu/LeaderBoard.java
+++ textedu/src/textedu/LeaderBoard.java
@@ -39,8 +39,9 @@ public class LeaderBoard {
 			if (userScores.get(i).equals(u)) {
 				int rank = i + 1;
 				int rankPrev = i;
+				
 				if (rankPrev == 0) {
-					toReturn += "Rank: " + rankPrev + " " + userScores.get(i).getName() + " " + userScores.get(i).getScore();
+					toReturn += "Rank: " + rankPrev + " " + userScores.get(i-1).getName() + " " + userScores.get(i-1).getScore() + "\n";
 				}
 				toReturn += "Rank: " + rank + " " + userScores.get(i).getName() + " " + userScores.get(i).getScore();
 			}
